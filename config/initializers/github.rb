e716baa924bd26ca4107f519c7a0409f1b1ffa8e
Florent Beaurain <beaurain.florent@gmail.com>
2020-04-05 19:08:05 +0200

    Change github access token ; change action mailer default from

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-GITHUB = Octokit::Client.new(access_token: 'f29cc11e2ff033003388f4c6fdcb57a67e1411f8')
+GITHUB = Octokit::Client.new(access_token: ENV.fetch('GITHUB_ACCESS_TOKEN'))
